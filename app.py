1a9b6d6895c23aa8984cf969b28e73d2db48e7f3
fabemiliano <fab.emiliano@gmail.com>
2024-05-18 11:22:54 -0300

    ˜wsgi˜

--- app.py
+++ app.py
@@ -285,4 +285,4 @@ def login():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=False, port=8000)
---
+++ myapp.wsgi
@@ -0,0 +1,4 @@
+import sys
+sys.path.append('./')
+
+from app.py import app as application
